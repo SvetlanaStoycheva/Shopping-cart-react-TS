b33eaa584a3d3c7050d01bff80983c7dacbf04c0
Svetlana Stoycheva <s.stoycheva@gmail.com>
2022-04-26 11:46:25 +0300

    shopping-cart-functionality

--- README.md
+++ README.md
@@ -2,11 +2,19 @@
 
 #### [tutorial>>>](https://www.youtube.com/watch?v=7NqeSf1c-bw)
 
+#### [see project](https://sweta-react-ts-shopping-cart.netlify.app)
+
+- Simple shopping cart home page. It uses Fake store API to load some random products on the home page, which have add to cart button. It has a shopping cart functionality with add/subtract item +/- buttons and total sum button. The app uses material-ui for easier components styling.
 - Libraries and API
   - material-ui
   - styled-components
   - react-query
   - [Fake store API](https://fakestoreapi.com)
+
+###### For Netlify upload
+
+- in package.json => in "scripts": => "build": "CI= react-scripts build"
+- in public => create new file \_redirects and inside: /\* /index.html 200
 
 #### Challenges
 
@@ -15,3 +23,12 @@
   - Next, rename any file to be a TypeScript file (e.g. src/index.js to src/index.tsx) and restart your development server!
 - Error "Cannot use JSX unless the '--jsx' flag is provided"
   - Adding "jsx": "preserve" to tsconfig.json will bypass the warning
+
+<p align-items: center>
+    <img src='./readme-images/Screenshot-image_01.png' width='250'>
+</p>
+<br/>
+<p align-items: center>
+    <img src='./readme-images/Screenshot-image_02.png' width='250'>
+</p>
+<br/>
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useQuery } from 'react-query';
 //Components
 import Item from './Item/Item';
+import Cart from './Cart/Cart';
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
@@ -37,18 +38,63 @@ const App = () => {
   // console.log(data);
   const getTotalItems = (items: CartItemType[]) =>
     items.reduce((acc: number, item) => acc + item.amount, 0);
-  const handleAddToCart = (clickedItem: CartItemType) => null;
-  const handleRemoveFromCart = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems((prev) => {
+      // 1. Is the item already added in the cart?
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+
+      if (isItemInCart) {
+        return prev.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
+      // First time the item is added
+      return [...prev, { ...clickedItem, amount: 1 }];
+    });
+  };
+  // const handleRemoveFromCart = (id: number) => {
+  //   setCartItems((prev) => {
+  //     prev.reduce((acc, curr) => {
+  //       if (curr.id === id) {
+  //         if (curr.amount === 1) return acc;
+  //         return [...curr, { ...curr, amount: curr.amount - 1 }];
+  //       } else {
+  //         return [...acc, curr];
+  //       }
+  //     }, [] as CartItemType[]);
+  //   });
+  // };
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems((prev) =>
+      prev.reduce((ack, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return ack;
+          return [...ack, { ...item, amount: item.amount - 1 }];
+        } else {
+          return [...ack, item];
+        }
+      }, [] as CartItemType[])
+    );
+  };
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Something went wrong...</div>;
   return (
     <Wrapper>
       <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
-        Cart goes here
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
       </Drawer>
       <StyledButton onClick={() => setCartOpen(true)}>
-        <Badge badgeContent={getTotalItems(cartItems)} color='error'></Badge>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
       </StyledButton>
       <Grid container spacing={3}>
         {data?.map((item) => (
